7c8c1a439a03f8a3f48d90161787396680396a9c
Khadidja Yasmine BOUREGA <bourega.khadidja.y@gmail.com>
2022-11-08 12:24:29 -0500

    Add files via upload

--- ift6758/data/Feature_Engineering1.py
+++ ift6758/data/Feature_Engineering1.py
@@ -18,12 +18,10 @@ def rinkSideMissingValues2 (df) -> pd.DataFrame:
     :return: return a dataframe  
     """
     # create nets coordinates 
-    df['X_net_left'] = -89 
     df['Y_net'] = 0 
-    df['X_net_rght'] = 89
     # calculate the distance of the shot from both nets
-    df['DistanceFromLeftNet'] = ((df["xCoord"] - df["X_net_left"])**2 + (df["yCoord"] - df["Y_net"])**2)**0.5
-    df['DistanceFromRightNet'] =  ((df["xCoord"] - df["X_net_rght"])**2 + (df["yCoord"] - df["Y_net"])**2)**0.5
+    df['DistanceFromLeftNet'] = ((df["xCoord"] - -89 )**2 + (df["yCoord"] - df["Y_net"])**2)**0.5
+    df['DistanceFromRightNet'] =  ((df["xCoord"] - 89)**2 + (df["yCoord"] - df["Y_net"])**2)**0.5
     
     #our new rinkside 
     df['new_rinkSide'] = np.where(df['DistanceFromLeftNet'] <df['DistanceFromRightNet'],'right','left')
@@ -43,11 +41,10 @@ def addDistanceFromNet (df)-> pd.DataFrame:
 
 def addAngleFromNet (df) -> pd.DataFrame:
     df = addDistanceFromNet (df)
-    df["Angle_from_net"]  = df.apply( lambda row :  math.degrees(math.atan2(row.yCoord , row.xCoord -row.X_net)) if 
-                                     (row.xCoord < 90)   else  math.degrees(math.atan2(row.yCoord , row.xCoord -row.X_net)),
+    df["Angle_from_net"]  = df.apply( lambda row :  math.degrees(math.atan2(abs(row.yCoord) , abs(row.X_net-row.xCoord))) if 
+                                     (row.xCoord < 90)   else  math.degrees(math.atan2(abs(row.yCoord) , abs(row.X_net-row.xCoord)))+90,
                                      axis=1 , result_type="expand" )
     return df
-        
 
 def combineAllFeatures (df) -> pd.DataFrame: 
     """Return a dataframe with Distance and angle of the shot from the net
@@ -55,4 +52,5 @@ def combineAllFeatures (df) -> pd.DataFrame:
     df1 = AddIsGoalFeature (df)
     df2 = AddEmptyNetFeature (df1)  
     df3 = addAngleFromNet (df2)
+    df3.Distance_from_net = df3.Distance_from_net.astype(float)
     return df3
