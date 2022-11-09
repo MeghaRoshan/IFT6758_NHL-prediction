382c2a3a02ee736f78500bb44fa85bf0802ae221
Khadidja Yasmine BOUREGA <bourega.khadidja.y@gmail.com>
2022-11-08 19:52:23 -0500

    Add files via upload

--- ift6758/data/Feature_Engineering1.py
+++ ift6758/data/Feature_Engineering1.py
@@ -42,7 +42,7 @@ def addDistanceFromNet (df)-> pd.DataFrame:
 def addAngleFromNet (df) -> pd.DataFrame:
     df = addDistanceFromNet (df)
     df["Angle_from_net"]  = df.apply( lambda row :  math.degrees(math.atan2(abs(row.yCoord) , abs(row.X_net-row.xCoord))) if 
-                                     (row.xCoord < 90)   else  math.degrees(math.atan2(abs(row.yCoord) , abs(row.X_net-row.xCoord)))+90,
+                                     (row.xCoord < 91)   else  math.degrees(math.atan2(abs(row.yCoord) , abs(row.X_net-row.xCoord)))+90,
                                      axis=1 , result_type="expand" )
     return df
 
@@ -54,3 +54,31 @@ def combineAllFeatures (df) -> pd.DataFrame:
     df3 = addAngleFromNet (df2)
     df3.Distance_from_net = df3.Distance_from_net.astype(float)
     return df3
+
+def goalRatioPerDistance (df,bins) :
+    df = df[['Distance_from_net','Is_goal']].dropna()
+    df['dist_binned'] = pd.cut(df['Distance_from_net'], bins)
+    df = df.groupby(['dist_binned', 'Is_goal']).agg('count').reset_index()
+    table = pd.pivot_table(df, columns=['Is_goal'], index=['dist_binned'],values='Distance_from_net',
+                    aggfunc='sum').reset_index()
+
+    table['total'] = table[0]+ table[1]
+    table = table[['dist_binned','total', 1 ]]
+    table['goalRatio'] = round(table[1]*100/ table['total'],2)
+    df_final = table [['goalRatio', 'dist_binned']].reset_index(drop =True).dropna()
+    df_final['dist_binned'] =df_final['dist_binned'].astype('str')
+    return df_final
+
+def goalRatioPerAngle (df,bins) :
+    df = df[['Angle_from_net','Is_goal']].dropna()
+    df['Anlge_binned'] = pd.cut(df['Angle_from_net'], bins)
+    df = df.groupby(['Anlge_binned', 'Is_goal']).agg('count').reset_index()
+    table = pd.pivot_table(df, columns=['Is_goal'], index=['Anlge_binned'],values='Angle_from_net',
+                    aggfunc='sum').reset_index()
+
+    table['total'] = table[0]+ table[1]
+    table = table[['Anlge_binned','total', 1 ]]
+    table['goalRatio'] = round(table[1]*100/ table['total'],2)
+    df_final = table [['goalRatio', 'Anlge_binned']].reset_index(drop =True).dropna()
+    df_final['Anlge_binned'] =df_final['Anlge_binned'].astype('str')
+    return df_final
