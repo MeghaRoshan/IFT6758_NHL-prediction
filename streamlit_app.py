e6614a3cae23e85542f500ca1c38f08ec6166ad9
Megha Roshan <44066009+MeghaRoshan@users.noreply.github.com>
2022-12-26 00:41:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
+import json
 import numpy as np
 from ift6758.ift6758.client.game_client import GameClient
 from ift6758.ift6758.client.serving_client import ServingClient
+import traceback
 
 
 from comet_ml import API
@@ -22,9 +24,9 @@ st.title("Hockey Visualization App")
 
 with st.sidebar:
     # TODO: Add input for the sidebar
-    workspace = st.text_input('Workspace', 'yasmine') #yasmine
-    model = st.text_input('Model', 'model-xgboost-final2') #"model-xgboost-final2"
-    version = st.text_input('Version', '1.0.0')#"1.0.0"
+    workspace = st.text_input('Workspace', 'Workspace x') #yasmine
+    model = st.text_input('Model', 'Model y') #"model-xgboost-final2"
+    version = st.text_input('Version', 'Version z')#"1.0.0"
     if st.button('Get Model'):
         print(workspace)
         print(model)
@@ -40,30 +42,7 @@ with st.sidebar:
             st.session_state['previous_track_download_model'] = 1
         else:
             st.session_state['track_download_model'] += 1
-'''
-with st.container():
-    # TODO: Add Game ID input
-    gameID = st.text_input('GameID')
-    if st.button('Ping Game'):
-        result, last_idx = pingGame(gameID)
-        st.write(result)
-    else:
-        st.write('Fill in the gameID')
-    pass
 
-with st.container():
-    # TODO: Add Game info and predictions
-    
-    heading="GameID"+""+gameID+":"+teams
-    st.title(heading)
-    pass
-
-with st.container():
-    # TODO: Add data used for predictions
-    st.title("Data used for predictions (and Predictions)")
-    st.write(result)
-    pass
-    '''
 def ping_game_id(game_id):
     with st.container():
         # Keep the game_id in session_state
@@ -94,13 +73,15 @@ def ping_game_id(game_id):
             #          "again with the new events set to get the predictions and avoid Features Mismatch Problem."
             #          "Please stop the application service and retrain your model again on new game id.")
         try:
+            
             if st.session_state['game_id'] == game_id:
                 # Get the filepath of the recent game_id downloaded json
                 #filepath = gc.get_game(game_id=game_id)
                 # Get the dataframe, last event, and dataframe_length
                 model_df, last_event_df_index, new_dataframe_length = gc.pingGame(game_id)
-                model_df.drop(model_df.columns[len(model_df.columns)-1], axis=1, inplace=True)
+                #model_df.drop(model_df.columns[len(model_df.columns)-1], axis=1, inplace=True)
                 last_event_df = model_df.iloc[-1]
+                
             # If the session ran already, then the session_tracker should update with
             # new length that is newly loaded dataframe length - old evaluated dataframe length
             if 'session_tracker' in st.session_state and st.session_state.session_tracker > 0 and st.session_state['game_id'] == game_id:
@@ -114,6 +95,7 @@ def ping_game_id(game_id):
                 st.session_state['previous_session_tracker'] = new_dataframe_length - st.session_state.previous_session_tracker
             # If current session and previous session have dataframe length then it has new events
             if st.session_state.session_tracker > 0 and st.session_state.previous_session_tracker > 0 and st.session_state['game_id'] == game_id:
+                
                 st.write("There are total " + str(st.session_state.session_tracker) + " new events!")
                 st.session_state['previous_session_tracker'] = new_dataframe_length - st.session_state.previous_session_tracker
             # If both session and previous session do not have the events then it load is first time for game id
@@ -125,6 +107,8 @@ def ping_game_id(game_id):
             if st.session_state.session_tracker > 0 and st.session_state.previous_session_tracker > 0 and st.session_state['game_id'] == game_id:
                 model_df = model_df.copy()
                 model_df = model_df.iloc[-st.session_state.session_tracker:]
+                new_model_df=model_df.drop(columns=['homeTeam','AwayTeam','Is_goal','teamOfShooter','periodTime'])
+                #print(new_model_df)
                 st.session_state.session_tracker = new_dataframe_length
                 st.session_state.previous_session_tracker = new_dataframe_length
             # Current and Previous session values will be equal once we will reach at the state of predicting the model
@@ -132,52 +116,59 @@ def ping_game_id(game_id):
             # so that in next run previous session will give us the value of this run
             if st.session_state.session_tracker == st.session_state.previous_session_tracker and st.session_state['game_id'] == game_id:
                 print("I am in Streamlit.....")
-                preds = sc.predict(model_df)
+                preds = sc.predict(new_model_df)
                 try:
+                    preds= preds.to_json()
                     preds = json.loads(preds)
-                    preds = [pred for pred in preds['0'].values()]
+                
+                    #preds = [pred for pred in preds['0'].values()]
+                    print(model_df)
+                    preds=list(preds["probaIsGoal"].values())
                     model_df["Model Output"] = preds
-                    model_df.is_goal = model_df.is_goal.astype(str)
+                    
+                    model_df.Is_goal = model_df.Is_goal.astype(str)
+                    
                     # Calculating the predict and actual goals probability
-                    grouped_prob_df = model_df.where(model_df.is_goal == "True").groupby("team_shot")["Model Output"].agg("sum").round(decimals=2)
-                    grouped_goal_df = model_df.where(model_df.is_goal == "True").groupby("team_shot")["is_goal"].count()
+                    grouped_prob_df = model_df.where(model_df.Is_goal == "1.0").groupby("teamOfShooter")["Model Output"].agg("sum").round(decimals=2)
+                    
+                    grouped_goal_df = model_df.where(model_df.Is_goal == "1.0").groupby("teamOfShooter")["Is_goal"].count()
+                    
                     grouped_prob_df = pd.DataFrame(grouped_prob_df).transpose()
                     grouped_goal_df = pd.DataFrame(grouped_goal_df).transpose()
                     last_event_df = pd.DataFrame(last_event_df).transpose()
                     last_event_df = last_event_df.reset_index()
                     # If the team did not acquire any goal then put zero goals for that team
                     if len(grouped_prob_df) < 2:
-                        if str(model_df.home_team[0]) not in grouped_prob_df.columns:
-                            grouped_prob_df[model_df.home_team[0]] = 0
-                        if str(model_df.away_team[0]) not in grouped_prob_df.columns:
-                            grouped_prob_df[model_df.away_team[0]] = 0
+                        if str(model_df.homeTeam[0]) not in grouped_prob_df.columns:
+                            grouped_prob_df[model_df.homeTeam[0]] = 0
+                        if str(model_df.AwayTeam[0]) not in grouped_prob_df.columns:
+                            grouped_prob_df[model_df.AwayTeam[0]] = 0
                     if len(grouped_goal_df) < 2:
-                        if str(model_df.home_team[0]) not in grouped_goal_df.columns:
-                            grouped_goal_df[model_df.home_team[0]] = 0
-                        if str(model_df.away_team[0]) not in grouped_goal_df.columns:
-                            grouped_goal_df[model_df.away_team[0]] = 0
+                        if str(model_df.homeTeam[0]) not in grouped_goal_df.columns:
+                            grouped_goal_df[model_df.homeTeam[0]] = 0
+                        if str(model_df.AwayTeam[0]) not in grouped_goal_df.columns:
+                            grouped_goal_df[model_df.AwayTeam[0]] = 0
 
                     # Calculating the time left in period
-                    time_remain_df = str(pd.DataFrame(pd.to_datetime("20:00", format="%M:%S")
-                                 - pd.to_datetime(last_event_df["period time"], format="%M:%S"))['period time'][0])
+                    #time_remain_df = str(pd.DataFrame(pd.to_datetime("20:00", format="%M:%S")
+                                 #- pd.to_datetime(last_event_df["periodTime"], format="%M:%S"))['periodTime'][0])
 
-                    st.subheader("Game " + str(game_id) + ": " + str(model_df.home_team[0]) + " vs " + str(model_df.away_team[0]))
-                    st.text("Period " + str(last_event_df.period[0]) + " - "
-                            + str(":".join(time_remain_df.split("days")[-1].split(":")[1:])) + " left")
+                    st.subheader("Game " + str(game_id) + ": " + str(model_df.homeTeam[0]) + " vs " + str(model_df.AwayTeam[0]))
+                    #st.text("Period " + str(last_event_df.period[0]) + " - "
+                         #   + str(":".join(time_remain_df.split("days")[-1].split(":")[1:])) + " left")
+                    
                     # Arranging the values in two columns
                     col1, col2 = st.columns(2)
-                    col1.metric(label=str(model_df.home_team[0]) + " xG(actual)", value=str(grouped_prob_df[model_df.home_team[0]][0])+" ("
-                                                                                      + str(grouped_goal_df[model_df.home_team[0]][0])
+                    col1.metric(label=str(model_df.homeTeam[0]) + " xG(actual)", value=str(grouped_prob_df[model_df.homeTeam[0]][0])+" ("
+                                                                                      + str(grouped_goal_df[model_df.homeTeam[0]][0])
                                                                                       + ")",
-                              delta=str(float(float(grouped_goal_df[model_df.home_team[0]][0])-float(
-                                  grouped_prob_df[model_df.home_team[0]][0])).__round__(2)))
-                    col2.metric(label=str(model_df.away_team[0]) + " xG(actual)",
-                              value=str(grouped_prob_df[model_df.away_team[0]][0]) + " ("
-                                    + str(grouped_goal_df[model_df.away_team[0]][0])
-                                    + ")",
-                              delta=str(float(float(grouped_goal_df[model_df.away_team[0]][0]) - float(
-                                  grouped_prob_df[model_df.away_team[0]][0])).__round__(2)))
-
+                    delta=str(float(float(grouped_goal_df[model_df.homeTeam[0]][0])-float(
+                                  grouped_prob_df[model_df.homeTeam[0]][0])).__round__(2)))
+                    col2.metric(label=str(model_df.AwayTeam[0]) + " xG(actual)",
+                              value=str(grouped_prob_df[model_df.AwayTeam[0]][0]) + " ("+ str(grouped_goal_df[model_df.AwayTeam[0]][0])+")",
+                              delta=str(float(float(grouped_goal_df[model_df.AwayTeam[0]][0]) - float(
+                                  grouped_prob_df[model_df.AwayTeam[0]][0])).__round__(2)))
+                    
                     with st.container():
                         # Fetching the entire dataframe
                         st.subheader("Data used for predictions (and predictions)")
@@ -186,11 +177,11 @@ def ping_game_id(game_id):
                     print(e)
                     pass
         except Exception as e:
-            st.write("Please turn on your prediction service.")
-            print(e)
-            print(traceback.format_exc())
-            st.session_state['session_tracker'] = 0
-            st.session_state['previous_session_tracker'] = 0
+                st.write("Please turn on your prediction service.")
+                print(e)
+                print(traceback.format_exc())
+                st.session_state['session_tracker'] = 0
+                st.session_state['previous_session_tracker'] = 0
 
 
 with st.container():
@@ -198,3 +189,5 @@ with st.container():
     game_id = st.text_input('Game ID', '2021020329')
     if st.button('Ping game'):
         ping_game_id(game_id)
+
+    
